462894d55565c57fafb5cafecdb14ac215acd55a
Aluoní <105504582+AluoniLopes@users.noreply.github.com>
2025-08-27 18:47:45 -0300

    Add relationships between Board, Column, and Card entities

--- src/main/java/edu/dio/persistence/entity/BoardEntity.java
+++ src/main/java/edu/dio/persistence/entity/BoardEntity.java
@@ -1,16 +1,37 @@
 package edu.dio.persistence.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static edu.dio.persistence.entity.BoardColumnKindEnum.CANCEL;
+import static edu.dio.persistence.entity.BoardColumnKindEnum.INITIAL;
+
 @Data
 public class BoardEntity {
     private Long id;
     private String name;
     @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private List<BoardColumnEntity> boardColumns = new ArrayList<>();
+
+
+    public BoardColumnEntity getInitialColumn(){
+        return filter(INITIAL);
+
+    };
+
+    public BoardColumnEntity getCancelColumn(){
+        return filter(CANCEL);
+    };
+
+    private BoardColumnEntity filter(final BoardColumnKindEnum kind){
+        return boardColumns.stream()
+                .filter(bc -> bc.getKind().equals(kind))
+                .findFirst().orElseThrow();
+    }
 }
--- src/main/java/edu/dio/persistence/entity/CardEntity.java
+++ src/main/java/edu/dio/persistence/entity/CardEntity.java
@@ -9,5 +9,6 @@ public class CardEntity {
     private Long id;
     private String title;
     private String description;
+    private BoardColumnEntity boardColumn;
 
 }
